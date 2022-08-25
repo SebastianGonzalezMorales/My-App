f3c290dee7c25b0dce2a6ba3a56f86fdcc3290ab
SebastianGonzalezMorales <sebastian.gonzalez@alumnos.uv.cl>
2022-08-25 18:34:39 -0400

    Subiendo login

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -2,14 +2,24 @@ const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+    const userList = await User.find().select('-passwordHash');
     
     if (!userList) {
         res.status(500).json({success: false})
     }
     res.send(userList);
+})
+
+router.get(`/:id`, async (req, res) =>{
+    const user = await User.findById(req.params.id.select('-passwordHash'));
+    
+    if (!user) {
+        res.status(500).json({message: 'The user with given ID was '})
+    }
+    res.status(200).send(user);
 })
 
 
@@ -33,6 +43,30 @@ router.post(`/`, async (req, res) =>{
     if (!user) 
         return res.status(400).send('the user cannot be created!')   
     res.send(user);
+})
+
+router.post('/login', async (req, res)=>{
+    const user = await User.findOne({email: req.body.email})
+    const secret = process.env.secret;
+
+    if(!user) {
+        return res.status(400).send('The user not found');
+    }
+
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign(
+            {
+                userId: user.id
+            },
+            secret,
+            {expiresI : '1d'}
+        )
+    
+        res.status(200).send({name: user.name, user: user.email, token: token})
+  
+    } else {
+        res.status(400).send('password is wrong!');
+    }
 })
 
 
