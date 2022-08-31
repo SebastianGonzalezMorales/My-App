09061f42f075904ec7ca603eec8fb297543ea8e3
SebastianGonzalezMorales <sebastian.gonzalez@alumnos.uv.cl>
2022-08-31 16:59:51 -0400

    Subiendo nuevamente Frontend y parte del Menú

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,20 +1,20 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+//Navigators
+import Main from './Navigators/Main'
+
+// Screens
+import ProductContainer from './Screens/Products/ProductContainer';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Main/>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
