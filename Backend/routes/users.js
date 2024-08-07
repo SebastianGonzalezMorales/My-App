562e2a4bcae0609fbce7cf2f5397e3d83dbe67bc
SebastianGonzalezMorales <Sebastian.gonzalez@alumnos.uv.cl>
2024-08-07 12:46:19 -0400

    Agregando funcionalidad de al user

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -61,6 +61,37 @@ router.get("/get-random-user", async (req, res) => {
     }
   });
   
+  router.get("/get-all-user", async (req, res) => {
+    try {
+      const data = await User.find({});
+      res.send({ status: "Ok", data: data });
+    } catch (error) {
+      return res.send({ error: error });
+    }
+  });
+
+
+  //Falta probar
+  router.post("/update-user", async (req, res) => {
+    const { name, email, password } = req.body;
+    console.log(req.body);
+    try {
+      await User.updateOne(
+        { email: email },
+        {
+          $set: {
+            name,
+            password
+          },
+        }
+      );
+      res.send({ status: "Ok", data: "Updated" });
+    } catch (error) {
+      return res.send({ error: error });
+    }
+  });
+
+
 
 /* router.get(`/`, async (req, res) =>{
     const userList = await User.find().select('-passwordHash');
@@ -105,45 +136,7 @@ router.get(`/:id`, async (req, res) =>{
 })
  */
 
-router.post('/login', async (req, res)=>{
-    const user = await User.findOne({email: req.body.email})
-    const secret = process.env.secret;
 
-    if(!user) {
-        return res.status(400).send('The user not found');
-    }
-
-    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
-        const token = jwt.sign(
-            {
-                userId: user.id,
-                isAdmin: user.isAdmin
-            },
-            secret,
-            {expiresIn : '1d'}
-        )
-        res.status(200).send({name: user.name, user: user.email, token: token})
-    } else {
-        res.status(400).send('password is wrong!');
-    }
-})
-
-
-router.post('/register', async (req, res)=>{
-    let user = new User({
-        name: req.body.name,
-        rut: req.body.rut,
-        email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
-        birthdate: req.body.birthdate,
-        carrera: req.body.carrera,
-        isAdmin: req.body.isAdmin
-    })
-    user = await user.save();
-    if (!user)
-        return res.status(400).send('The user cannot be created !')
-    res.send(user);
-})
 
 /* router.get(`/get/count`, async (req, res) =>{
     const userCount = await User.countDocuments((count) => count)
